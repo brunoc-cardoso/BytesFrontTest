b148ccf41b353fe1217680e227ddc9f387db2695
bruno.cardoso <brunocpessoal@gmail.com>
2019-01-22 17:27:29 -0200

    component added and completed sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { BlocksComponent } from './blocks/blocks.component';
 import { IconBarComponent } from './icon-bar/icon-bar.component';
 import { TimerComponent } from './timer/timer.component';
+import { SidebarComponent } from './navigation/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlocksComponent,
     IconBarComponent,
-    TimerComponent
+    TimerComponent,
+    SidebarComponent
   ],
   imports: [
     NgbModule,
