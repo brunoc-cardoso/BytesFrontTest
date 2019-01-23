c77ebd26f1e66b81fd2190d2fc423f4f8fe561d7
bruno.cardoso <brunocpessoal@gmail.com>
2019-01-22 22:33:47 -0200

    interface layout worked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { BlocksComponent } from './blocks/blocks.component';
-import { IconBarComponent } from './icon-bar/icon-bar.component';
+import { IconBarComponent } from './navigation/icon-bar/icon-bar.component';
 import { TimerComponent } from './timer/timer.component';
 import { SidebarComponent } from './navigation/sidebar/sidebar.component';
 
--- src/app/navigation/icon-bar/icon-bar.component.ts
+++ src/app/navigation/icon-bar/icon-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faBell, faCalendar, faComments, faUser, faCog } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-icon-bar',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./icon-bar.component.css']
 })
 export class IconBarComponent implements OnInit {
+
+  faBell = faBell;
+  faCalendar = faCalendar;
+  faComments = faComments;
+  faUser = faUser;
+  faCog = faCog;
 
   constructor() { }
 
